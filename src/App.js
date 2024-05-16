5ced0a6c967cd5541a3be34e439bc518d60e3225
DwayneA <dwaynearpin@gmail.com>
2024-05-16 15:25:28 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Dwayne Arpin's portfolio website is in the making.
         </p>
         <a
           className="App-link"
